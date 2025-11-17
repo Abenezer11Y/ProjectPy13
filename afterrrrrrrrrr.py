b4b5998ba4ba4e81ddfbdc190d194e9ffe2f661e
Abenezer11Y <abenezerminale7@gmail.com>
2025-11-17 18:03:54 +0300

    Add files via upload

---
+++ afterrrrrrrrrr.py
@@ -0,0 +1,87 @@
+print("Welcome!")
+
+gradelvl = int(input("What grade are you in(from 1-10 only)? "))
+age = int(input("How old are you? "))
+
+if gradelvl == 1:
+    if age == 7:
+        print("That is average.")
+    elif age >= 8:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 2:
+    if age == 8:
+        print("That is average.")
+    elif age >= 9:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 3:
+    if age == 9:
+        print("That is average.")
+    elif age >= 10:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 4:
+    if age == 10:
+        print("That is average.")
+    elif age >= 11:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 5:
+    if age == 11:
+        print("That is average.")
+    elif age >= 12:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 6:
+    if age == 12:
+        print("That is average.")
+    elif age >= 13:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 7:
+    if age == 13:
+        print("That is average.")
+    elif age >= 14:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 8:
+    if age == 14:
+        print("That is average.")
+    elif age >= 15:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 9:
+    if age == 15:
+        print("That is average.")
+    elif age >= 16:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl == 10:
+    if age == 16:
+        print("That is average.")
+    elif age >= 17:
+        print("That is above average.")
+    else:
+        print("This is below average.")
+
+if gradelvl > 10:
+    print("Only grades from 1-10")
